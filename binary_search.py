340da6e4ac57a0f43d404c6233b55d5de198d0ca
nor5 <noorbelkacem5@gmail.com>
2022-05-17 15:15:11 +0200

    first commit

---
+++ binary_search.py
@@ -0,0 +1,38 @@
+import random
+import time
+def binary_search (list, target, low=None, high=None):
+    if low is None:
+        low = 0
+        
+    if high is None:
+        high = len(list) - 1
+        
+    if high < low:
+        return -1
+    midpoint = (high + low) // 2
+##    print(f"midpoint {midpoint}")
+    if target == list[midpoint]:
+        return midpoint
+    elif target < list[midpoint]:
+##        print(f"min {list[low:midpoint - 1]}")
+        return binary_search (list, target, low, midpoint - 1)
+    else:
+##        print(f" max {list[midpoint +1: high]}")
+        return binary_search (list, target, midpoint + 1, high)
+
+
+
+length = 10000
+    # build a sorted list of length 10000
+sorted_list = set()
+
+while len(sorted_list) < length:
+    sorted_list.add(random.randint(-3*length, 3*length))
+    
+sorted_list = sorted(list(sorted_list))
+start = time.time()
+for target in sorted_list:
+     binary_search(sorted_list ,23)
+
+end = time.time()
+print(f"binary search time: {end-start} seconds")
